2eb4e6ea6489c387bfb27f0b095bc11ae4aebea1
ddubovskyy <ddubovskyy@gmail.com>
2021-02-22 02:01:53 +0100

    [GitHub flow - 3]

--- cypress/integration/tests/flowGitHub_spec.js
+++ cypress/integration/tests/flowGitHub_spec.js
@@ -1,22 +1,42 @@
+var repos = []
+
 describe("", ()=> {
 
     it('Get all of my repos', () => {
         cy.request( "https://api.github.com/users/dendub/repos").then((response) => {
             expect(response).to.have.property('status', 200)
-            expect(response.body).to.have.length(14) //проверка эрэя репорзиторий на колво элементов
-            expect(response.body[2]).to.not.be.null //проверка существует ли второй элемент эрэя
-            expect(response.body[2].name).to.equal("cypress_tests") //проверка является ли он репозиторией "cypress_tests"
-
+            for (let i =0; i < 14; i++ ) {
+                expect(response.body[i]).to.not.be.null // выводит все репы
+                expect(response.body[i].name).to.exist
+                repos[i] = response.body[i].name // записывает название репозиториев в эррэй
+            }
+            // for (let i =0; i < 14; i++) {
+            //     console.log(repos[i])
+            // }
         })
+
     })
 
+    
 
-    it("Get all of the branches of the cypress_tests repo", () => {
-        cy.request("https://api.github.com/repos/dendub/cypress_tests/branches").then((response) => {
-            expect(response).to.have.property('status', 200)
+
+
+    it("Show branches of each repository", () => {
+        for ( let i =0; i<14; i++) {
+            cy.request("https://api.github.com/repos/dendub/" + repos[i] + '/branches').then((response) => {
             expect(response).to.have.property('status', 200)
 
         })
+        }
+        cy.request("https://api.github.com/repos/dendub/cypress_tests/branches/dev").then((response) => {
+            expect(response).to.have.property('status', 200)
+            
 
+        })
+        // cy.request("https://api.github.com/repos/dendub/cypress_tests/commits/dev").then((response) => {
+        //     expect(response).to.have.property('status', 200)
+        //     expect(response).to.have.property('status', 200)
+
+        // })
     })
 })
